de3430fc4f665fb0ddc7b5a147f9c140c6dee4b8
12101111 <w12101111@gmail.com>
2019-03-29 14:52:40 +0800

    refactor

---
+++ src/kmain.rs
@@ -0,0 +1,6 @@
+pub fn kmain() -> ! {
+    println!("Hello,World !");
+    println!("Boot from UEFI !!");
+    println!("Written in Rust !!!");
+    panic!("Good bye!");
+}
--- src/uefi_main.rs
+++ src/uefi_main.rs
@@ -1,14 +1,12 @@
-#![no_std]
-#![no_main]
-use fbterm::*;
-use spin::Mutex;
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
 use uefi::prelude::*;
 use uefi::proto::console::gop::GraphicsOutput;
 use uefi::table::boot::{MemoryDescriptor, MemoryType};
-
-static CONSOLE: Mutex<Option<Fbterm<XRGB8888>>> = Mutex::new(None);
-
-const TEXT: &str = "Hello,World !\nBoot from UEFI !!\nWritten in Rust !!!";
+#[macro_use]
+mod drivers;
+mod kmain;
+use drivers::fb::init_fb;
 
 #[no_mangle]
 pub extern "C" fn efi_main(image: uefi::Handle, st: SystemTable<Boot>) -> ! {
@@ -29,24 +27,9 @@ pub extern "C" fn efi_main(image: uefi::Handle, st: SystemTable<Boot>) -> ! {
     let (st, _iter) = st
         .exit_boot_services(image, &mut mmap_storage[..])
         .expect_success("Failed to exit boot services");
-    let rt = unsafe { st.runtime_services() };
+    let _rt = unsafe { st.runtime_services() };
     init_fb(fb.as_mut_ptr(), width, height);
-    println!("{}", TEXT);
-    let time = rt.get_time().expect_success("Failed to get time");
-    println!("Time: {}-{}-{} {}:{}:{}",time.year(),time.month(),time.day(),time.hour(),time.minute(),time.second());
-    panic!("Good bye!");
-}
-
-fn init_fb(fb: *mut u8, width: usize, height: usize) {
-    let background = XRGB8888::new(0, 0, 0, 0xA8);
-    let foreground = XRGB8888::new(255, 0xA8, 0xA8, 0xA8);
-    let fb = unsafe { Framebuffer::new(fb, width, height, background, foreground) };
-    let mut fbterm = Fbterm::new(fb,fbterm::Fonts::VGA8x14);
-    fbterm.clear();
-    let mut console = CONSOLE.lock();
-    if console.is_none() {
-        *console = Some(fbterm)
-    }
+    kmain::kmain();
 }
 
 #[cfg(not(debug_assertions))]
@@ -77,22 +60,7 @@ fn set_graphics_mode(gop: &mut GraphicsOutput) -> (usize, usize) {
     mode.info().resolution()
 }
 
-pub fn _print(args: core::fmt::Arguments) {
-    use core::fmt::Write;
-    CONSOLE.lock().as_mut().unwrap().write_fmt(args).unwrap();
-}
-
-#[macro_export]
-macro_rules! print {
-    ($($arg:tt)*) => ($crate::_print(format_args!($($arg)*)));
-}
-
-#[macro_export]
-macro_rules! println {
-    () => ($crate::print!("\n"));
-    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
-}
-
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("{}", info);
